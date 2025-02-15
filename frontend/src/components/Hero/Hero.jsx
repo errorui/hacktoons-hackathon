7018d2550f36857fabb99bb037ff06d9de7c3097
r <rajraman21211@gmail.com>
2025-02-16 04:17:04 +0530

    finale2

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./HeroSection.css";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
@@ -8,8 +9,16 @@ const HeroSection = () => {
         <h1>Invest in a greener future</h1>
         <p>Make an impact while growing your wealth</p>
         <div className="hero-buttons">
-          <button className="btn green-btn">Get Started</button>
-          <button className="btn white-btn">How It Works →</button>
+          <button className="btn green-btn"> 
+           <Link style={{
+            background:"blue",
+            color:"white"
+           }} to="/signup">Get Started</Link> </button>
+          <button className="btn white-btn">
+            <Link style={{
+            background:"white",
+            color:"blue"
+           }}>How It Works →</Link></button>
         </div>
       </div>
 
